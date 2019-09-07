2212a0da8bad30777cc495e0e10024586e93dc29
Ubuntu <ubuntu@ip-172-31-30-187.ec2.internal>
2019-09-07 17:45:20 +0000

    created respnse from genius api token

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import flask
 import os
 import random
+import requests, requests_oauthlib
+import json
 
 app = flask.Flask(__name__)
+url = "https://api.genius.com/search?q=Ari%20Lennox"
+my_headers = { 
+    "Authorization":"Bearer nW7AUdiUQgMX_-J2ThxfjdLeoef9ITdMdTAsPu38EckkKZUFyld95Byrn9MNGmUh"
+    
+}
+response = requests.get(url, headers=my_headers)
+json_body = response.json()
+print(json_body)
 
 @app.route('/')
 def index():
