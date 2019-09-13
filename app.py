96f10d522b89ab1e7ba60f768fef1f6ee48c556e
Ubuntu <ubuntu@ip-172-31-27-59.ec2.internal>
2019-09-13 07:02:16 +0000

    added twitter apis and auth

--- app.py
+++ app.py
@@ -9,20 +9,38 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    url = "https://api.genius.com/search?q=Ari%20Lennox"
-    auth = os.getenv('Genius_Authorization')
-    my_headers = { 
-        "Authorization":"Bearer "+ auth
+    gen_url = "https://api.genius.com/search?q=Ari%20Lennox"
+    twit_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=arilennox&include_rts=false"
+    gen_auth = os.getenv('Genius_Authorization')
+    twit_key = os.getenv('Twitter_Key')
+    twit_secret = os.getenv('Twitter_Secret')
+    twit_token = os.getenv('Twitter_Token')
+    twit_token_secret = os.getenv('Twitter_Token_Secret')
+    twit_oauth = requests_oauthlib.OAuth1(
+        twit_key,
+        twit_secret,
+        twit_token,
+        twit_token_secret,
+    )
+    genius_headers = { 
+        "Authorization":"Bearer nW7AUdiUQgMX_-J2ThxfjdLeoef9ITdMdTAsPu38EckkKZUFyld95Byrn9MNGmUh"
     }
-    response = requests.get(url, headers=my_headers)
-    json_body = response.json()
+    gen_response = requests.get(gen_url, headers=genius_headers)
+    twit_response = requests.get(twit_url, auth=twit_oauth)
+    gen_json_body = gen_response.json()
+    twit_json_body = twit_response.json()
     
-    random_index = random.randint(0, len(json_body['response']['hits'])-1)
+    gen_random_index = random.randint(0, len(gen_json_body['response']['hits'])-1)
+    twit_random_index = random.randint(0, len(twit_json_body)-1)
+    
+    print(json.dumps(twit_json_body, indent=2))
+
     return flask.render_template(
         "ari_page.html", 
-        song_name = json_body['response']['hits'][random_index]['result']['title_with_featured'], 
-        song_url = json_body['response']['hits'][random_index]['result']['url'], 
-        song_art = json_body['response']['hits'][random_index]['result']['header_image_thumbnail_url']
+        song_name = gen_json_body['response']['hits'][gen_random_index]['result']['title_with_featured'], 
+        song_url = gen_json_body['response']['hits'][gen_random_index]['result']['url'], 
+        song_art = gen_json_body['response']['hits'][gen_random_index]['result']['header_image_thumbnail_url'],
+        tweet_id = str(twit_json_body[twit_random_index]['id'])
     )
 
 app.run(
